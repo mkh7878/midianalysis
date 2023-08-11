56e3f03076eccf7e445fa86c4d74e02c668c5dbc
Mae Horak <70494117+mkh7878@users.noreply.github.com>
2023-08-11 15:59:58 +0100

    Add files via upload

--- determine_key.py
+++ determine_key.py
@@ -63,15 +63,50 @@ def musicmusic():
                 # in_between_time.append(in_between_times * 1000)
                 # print (in_between_time)
 
+#determining key based on  Krumhansl and Kessler’s (1982) study of tonal organization
+#essentially, the key is determined based on which note is played the most
+#then, we see if the magor or minor 3rd has been played more to determine if it's a minor or magor key
+#this is a very simplified version of the theory, and will become more complex as the project grows
 def determine_key():
     time.sleep(10)
-    #captured_notes is a list of all the notes caputured
-    for idx in captured_notes:
-        #get the note being played and add it to the interval_recurrance
-        if captured_notes[idx] in intervals.interval_recurrance:
-            intervals.interval_recurrance[idx] += 1
 
-    print(intervals.interval_recurrance)
+    #captured_notes is a list of all the notes captured
+    for note in range(len(captured_notes)):
+        #which note should be updated?
+        note_to_tally = captured_notes[note]
+        #use note value to index into inthervals_recurrance_list
+        #basically, tally up how many times the note has been used.
+        intervals.interval_recurrance_list[note_to_tally] += 1
+
+    #holds the number of times a note is played, and changes when one higher is found
+    how_many_times = 0
+    #variable to hold the index number for whichever note is played the most
+    which_note = 0
+
+    #go through interval_recurrance_list and determine which note has been played the most
+    for tally in range(12):
+        if intervals.interval_recurrance_list[tally] > how_many_times:
+            how_many_times = intervals.interval_recurrance_list[tally]
+            which_note = tally
+
+    likely_key = intervals.notes[which_note]
+
+    print('the most played note is:', likely_key, 'played', how_many_times, 'times.')
+
+
+    #set that note as the key
+    #figure out whether the minor or magor 3rd has been played more often
+    #determines if it's a magor or minor key
+    #eventually we will need to add check points to see if the actual notes being played match up with the key
+    #I think this should be checked every 30 seconds?
+
+    print(intervals.interval_recurrance_list)
+
+    # for value in intervals.interval_recurrance.values():
+    #     print(value, ':', end='')
+    #     for _ in range(intervals.interval_recurrance[value]):
+    #         print('x', end='')
+    #     print()
 
 if __name__ == '__main__':
     # Start the music function in a separate thread
@@ -81,5 +116,3 @@ if __name__ == '__main__':
     # Start the determine_key function in a separate thread
     key_thread = threading.Thread(target=determine_key)
     key_thread.start()
-
-
